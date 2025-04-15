464e9dbb1f749726e6a777ef5f4ac5dfeea16b50
SharadJ19 <sharadchandel2005@gmail.com>
2025-04-15 19:53:59 +0530

    Added correct cors

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
+const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
+
 const db = require('./configs/db');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -24,12 +31,6 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
   }
 })();
 
-const app = express();
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
 
 app.use(express.json());
 app.use(cookieParser());
